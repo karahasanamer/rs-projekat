084a7698a3af0fe8fb321aaf5e9f462e9230b4e1
karahasanamer <akarahasan4@etf.unsa.ba>
2020-06-03 15:10:17 +0200

    Pocetni ekran Login(Prijava) kreirana, funkcionalnost nije napravljena.

--- src/project/doctors/office/Main.java
+++ src/project/doctors/office/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        primaryStage.setTitle("Doctors office");
+        primaryStage.setScene(new Scene(root, 600, 300));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
